453f07d9aa867e591411a97786af486e97ce18df
rashonmitchell <rashonmitchell313@gmail.com>
2022-12-07 14:15:59 -0600

    adding operations to the arrays.js

---
+++ DSA/Array.js
@@ -0,0 +1,152 @@
+/** 
+ * Push() -
+ *  method adds a new element to the end of an array and returns the new array length.
+ * 
+ * Complexity:
+ *  push() operation will be O(1) since we only have to update the length of the array and add the new element.
+*/
+
+
+
+
+
+/**
+ * Pop() -
+ *  method removes the last element from an array and returns that element.
+ *  
+ * Complexity: 
+ *  pop() operation will be O(1) since we only have to update the length of the array and remove the end index.
+ */
+
+
+
+
+
+ /**
+ * Shift() -
+ *  method removes the first element from an array and returns that element.
+ *  
+ * Complexity: 
+ *  shift() operation will be O(n) since we have to remove the element at index 0, 
+ *  shift all the elements to the left, and then update the length of an array.
+ */
+
+
+
+
+
+/**
+ * Unshift(value) -
+ *   method adds one or more elements to the front of an array and returns the new array length.
+ *  
+ * Complexity: 
+ *  unshift() operation will be O(n) since we have to update the length of an array,
+ *  shift all the elements to the right, and then add the new element at index 0.
+ */
+
+
+
+
+
+/**
+ * delete keyword -
+ *  Elements can be deleted using delete in JavaScript. However, using delete leaves 
+ *  undefined holes in the array, so it is better to use the shift() or pop() method.
+ *  
+ * Complexity: 
+ *  delete() function will be O(n) To remove the first element from the beginning, we
+ *  need to move all the following elements to the left.
+ */
+
+
+
+
+
+/**
+ * Reverse() -
+ *  function reverses an array. Use it like array.reverse().
+ *  
+ * Complexity: 
+ *  reverse() function will be O(n) since we have to traverse all the elements of an array.
+ *  
+ */
+
+
+
+
+
+/**
+ * Splice() -
+ *  function can be used to add or remove elements. The first parameter is the index number, 
+ *  where the new elements should be spliced in.
+ * 
+ *  The second parameter is the number of elements that should be removed. 
+ *  If you don’t want to remove anything, just put 0 as the second parameter. 
+ * 
+ *  The rest of the parameters are the new elements to be added.
+ *  
+ * Complexity: 
+ *  splice() function is O(n) since we have to traverse all the elements of an array. 
+ *  Since splice() add or remove elements in the original array. Therefore, in the worst-case, 
+ *  we have to shift n-1 elements to the right.
+ *  
+ */
+
+
+
+
+
+/**
+ * Slice() -
+ *  function can be used to slice out a piece of an array into a new array. 
+ * 
+ * The first parameter is the index number, where the start of the slicing takes place.
+ * 
+ * The second parameter is the index number up to which the array should be sliced out.
+ * The new array excludes the second parameter. If the second parameter is left empty, 
+ * the new slice contains the elements from the first parameter to the rest of the array.
+ * 
+ *  
+ * Complexity: 
+ *  slice() function is O(n) Since slice() returns a new array
+ *  from the start to the end. Therefore, in the worst-case, 
+ *  we have to make a copy of all the elements present in the array.
+ *  
+ */
+
+
+
+
+
+/**
+ * Concat() -
+ *  function concatenates arrays as the name implies, and creates a 
+ *  new array which does not affect the source arrays.
+ * The first parameter is the index number, where the start of the slicing takes place.
+ * 
+ * The second parameter is the index number up to which the array should be sliced out.
+ * The new array excludes the second parameter. If the second parameter is left empty, 
+ * the new slice contains the elements from the first parameter to the rest of the array.
+ * 
+ *  
+ * Complexity: 
+ *  concat() function iwill be O(m+n), where m is the size of the first array, 
+ *  and n is the size of the second array.
+ * 
+ *  concat() makes a new array of size m+n and then copy elements of both arrays one by one in a new array.
+ *  
+ */
+
+
+
+
+
+/**
+ * for....of statement -
+ *  creates a loop to iterate over iterable objects, such as Arrays and Array-like objects.
+ *  
+ * Complexity: 
+ *  for...of will be O(n) where n is the number of items in an array.
+ *  
+ */
+
