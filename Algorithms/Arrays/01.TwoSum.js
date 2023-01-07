baa76f8e261b2a5ff6607936c3e1c943eba4b910
GPV <gvill005@gmail.com>
2023-01-06 23:28:00 -0800

    maxOperations

---
+++ Algorithms/Arrays/01.TwoSum.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * [1] Two Sum
+ * 
+ * https://leetcode.com/problems/two-sum/
+ * 
+ *  Given an array of integers nums and an integer target,
+ *  return indices of the two numbers such that they add up to target.
+ *
+ *  You may assume that each input would have exactly one solution,
+ *  and you may not use the same element twice.
+ * 
+ * 
+ *  Companies:
+ *  adobe | airbnb | amazon | apple | bloomberg | dropbox | facebook | linkedin | microsoft | uber | yahoo | yelp
+ *
+ *  Example 1:
+ *  Input: nums = [2,7,11,15], target = 9
+ *  Output: [0,1]
+ *  Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+ *
+ *  Example 2:
+ *  Input: nums = [3,2,4], target = 6
+ *  Output: [1,2]
+ *
+ *  Example 3:
+ *  Input: nums = [3,3], target = 6
+ *  Output: [0,1]
+ *
+ * */
+
+
+const twoSum = (nums, target) => {
+    // create hashmap
+
+    // iterate over num
+
+        // create to the value target - nums[idx]  ex. target = 9, nums[idx] = 2
+}
--- Algorithms/Arrays/1679.MaxNumberOfK-SumPairs.js
+++ Algorithms/Arrays/1679.MaxNumberOfK-SumPairs.js
@@ -44,89 +44,101 @@
  */
 
 // Two pointers sorted array
-// time complexity: O(nlogn)
+// time complexity: O(n log n)
 // space complexity: O(1)
 const maxOperations = function(nums, k) {
-    let count = 0;
-    let left = 0;
-    let right = nums.length - 1;
-    nums.sort((a, b) => a - b);
-    while (left < right) {
-        const sum = nums[left] + nums[right];
-        if (sum === k) {
-        count++;
-        left++;
-        right--;
+                                      //s
+    // create variable      // nums = [[1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 5]], k = 2     
+                                                //e
+    let result = 0, start = 0, end = nums.length - 1;
+    // sort nums 
+    nums.sort((a, b) => a-b);
+    // let arr = [4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4]
+    // arr.sort((a, b) => a-b)
+    // console.log("arr: ", arr) 
+    // console.log("nums", nums)
+    // iterate 
+    while(start < end) {
+        //  store sum value
+        let sum = nums[start] + nums[end];  
+        // if-cond
+        if( sum === k) {
+            // console.log("sum === k", sum === k, result, start, end)
+            // increase counter
+            result++;
+            // move start pointer
+            start++;
+            // move end pointer
+            end--;
         } else if (sum < k) {
-        left++;
+            // console.log("sum < k", sum < k, start)
+            start++;
         } else {
-        right--;
+            // console.log("sum > k", sum > k, end)
+            end--;
         }
     }
-    return count;
+    // return result
+    return result;
 };
+let arr = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 5], k = 2
 
-// another solution
+console.log("maxOperations: ", maxOperations([[4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4]], 2)) 
+// https://openai.com/blog/chatgpt/
 
-// Hashmap
-// time complexity: O(n)
-// space complexity: O(n)
-const maxOperations = function(nums, k) {
-    const map = {};
-    let count = 0;
-    for (let i = 0; i < nums.length; i++) {
-        const num = nums[i];
-        if (map[k - num]) {
-        count++;
-        map[k - num]--;
+//  another
+
+
+// hashmap w/ new Map()
+// time: O(n), space: O(n)
+const maxOperations2 = function(nums, k) {
+    let map = new Map(), counter = 0;
+    for(let element of nums) {
+        if(map.has(k - element)) {
+            counter++;
+            map.set(k - element, map.get(k - element) - 1) 
+
+            if(map.get(k - element) === 0) map.delete(k - element);   // ex 3 - 3 = 0
         } else {
-        map[num] = (map[num] || 0) + 1;
+            map.set(element, (map.get(element) || 0) + 1)
         }
     }
-    return count;
+    return counter;
+    // if(map[k - num]) {
+    //     map[num] = (map[num] || 0) + 1
+    // }
+
+    // for(let foo in map) {
+    //     if(map[k - num]) {
+    //         map[num] = (map[num] || 0) + 1
+    //     }
+    // }
 }
+console.log("maxOperations2: ", maxOperations2([3,1,3,4,3], 6)) 
 
-// another solution
 
-// Hashmap with for-loop
-// time complexity: O(n)
-// space complexity: O(n)
-const maxOperations = function(nums, k) {
-    const map = new Map();
-    let count = 0;
-    for (let i = 0; i < nums.length; i++) {
-        const num = nums[i];
-        if (map.has(k - num)) {
-        count++;
-        map.set(k - num, map.get(k - num) - 1);
-        if (map.get(k - num) === 0) {
-            map.delete(k - num);
-        }
-        } else {
-        map.set(num, (map.get(num) || 0) + 1);
-        }
-    }
-    return count;
-};
 
-// another solution
+// another 
 
-// Hashmap with for-of loop
-// time complexity: O(n)
-// space complexity: O(n)
-const maxOperations = function(nums, k) {
-    const map = new Map();
-    let count = 0;
-    for (let element of nums) {
-        if (map.has(k - element)) {
-        count++;
-        map.set(k - element, map.get(k - element) - 1);
-        if (map.get(k - element) === 0) {
-            map.delete(k - element);
-        }
-        } else {
-        map.set(element, (map.get(element) || 0) + 1);
+// hashmap
+// time: O(n), space: O(n)
+const maxOperations3 = function(nums, k) {
+    let map = {}, counter = 0;
+    for(let idx = 0; idx < nums.length; idx++) {
+        let num = nums[idx] // current number ex 3, 1, 3, 4, 3
+        // if the number that can make a pair exists in the map
+        if(map[k - num]) { // ex 3 - 3 = 0
+            // increase the number of pairs that can make k by 1 ex, 3+3 
+            counter++; // 1 -> 0 -> 1
+            // decrease the number that can make a pairs by 1
+            map[k - num]--; // decrease by 1
+        } else { // if the number that can make a pair doesn't exist
+            // increaing the count of the current number by 1
+            map[num] = (map[num] || 0) + 1; // map[3] = 2
         }
     }
-    return count;
-};
+    return counter;
+}   
+// { '1': 1, '3': 3, '4': 1 }
+console.log("maxOperations3: ", maxOperations3([3,1,3,4,3], 6)) 
+//                                                 
