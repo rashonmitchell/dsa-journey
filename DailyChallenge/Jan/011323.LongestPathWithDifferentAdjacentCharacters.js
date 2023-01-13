afc12d2c2240c85f174da3d8a4dbe7c434702fa1
rashonmitchell <rashonmitchell313@gmail.com>
2023-01-12 22:19:17 -0600

    adding [1519] Number of Nodes in the Sub-Tree With the Same Label

---
+++ DailyChallenge/Jan/011323.LongestPathWithDifferentAdjacentCharacters.js
@@ -0,0 +1,32 @@
+/** 
+ *
+ * [1519] Number of Nodes in the Sub-Tree With the Same Label
+ *
+ * https://leetcode.com/problems/longest-path-with-different-adjacent-characters/
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+/**
+ * @param {number[]} parent
+ * @param {string} s
+ * @return {number}
+ */
+
+const longestPath = function(parent, s) {
+    
+};
