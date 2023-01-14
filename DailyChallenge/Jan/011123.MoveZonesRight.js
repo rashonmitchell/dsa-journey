53622ff778febe4938321e07fdd4d8ebb2ad8c87
rashonmitchell <rashonmitchell313@gmail.com>
2023-01-13 21:51:04 -0600

    adding move zones right target

---
+++ DailyChallenge/Jan/011123.MoveZonesRight.js
@@ -0,0 +1,47 @@
+/** 
+ *
+ * Move Zeroes Right
+ *
+ * Given an array nums and an integer target, you need to move the element which equal to target to the front of the array,
+ * and the order of the remain elements can not be changed.
+ * 
+ * 
+ * All your move operations should be performed on the original array.
+ * 
+ * Note that you must do this in-place without making a copy of the array.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [5, 1, 6, 1], target = 1
+ * Output: [1, 1, 5, 6]
+ * Explanation: 1 is target, so you should keep them in the front of array.
+ * 
+ * Example 2:
+ * Input: nums = [-1, 2, 3, 5, 2, 2] target = 2
+ * Output: [2, 2, 2, -1, 3, 5]
+ * Explanation: 2 is target, so you should keep them in the front of array.
+ * 
+ * 
+ * Example 3:
+ * Input: nums = [2, 3, 4, 6] target = 1
+ * Output: [2, 3, 4, 6]
+ * Explanation: 1 is not target, so you should not change the array.
+ * 
+ * Constraints:
+ * The length of array is within range: [1, 100000]
+ * If the target does not appear in the array, there is no need to modify the original array.
+ * 
+ * 
+ * 
+ * Follow up: Could you minimize the total number of operations done?
+ */
+
+
+/**
+ * @param nums: a list of integer
+ * @param target: an integer
+ * @return: nothing
+ */
+
+
+const moveTarget = (nums, target) => {};
