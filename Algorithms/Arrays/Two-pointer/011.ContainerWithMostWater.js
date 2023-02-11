fe12e7eca01df6177fa808fe14ddbd9b8a349cb3
GPV <gvill005@gmail.com>
2023-02-11 10:39:21 -0800

    brute force solution for Conatiner with most water problem

--- Algorithms/Arrays/Two-pointer/011.ContainerWithMostWater.js
+++ Algorithms/Arrays/Two-pointer/011.ContainerWithMostWater.js
@@ -76,3 +76,43 @@ const maxArea2 = function(height) {
 
 // Greedy approach:
 const maxArea3 = function(height) {};
+
+
+
+
+// BRUTE FORCE APPROACH
+/* 
+    Understand the Problem:
+        Find the indices whose values create the largest area. Output the largest area.
+    Devise a Plan:
+        Brute Force
+        Use nested loops to find area between two indexes and its values. 
+        use a holder variable to store current highest number. at end of loops last number remains
+        return highest number
+    Code: Brute Force
+                                                         i
+                                        Input: height = [1,8,6,2,5,4,8,3,7]
+                                                             j
+Output: 49
+*/ 
+const maxArea4 = function(height) {
+    //create max area holder
+    let maxArea = 0;
+    //use nested loops to work with values
+    for(let i = 0; i < height.length; i++){
+        for(let j = i + 1; j < height.length; j++){
+            let area1 = (j - i) * height[i];
+            let area2 = (j - 1) * height[j];
+            if(height[i] <= height[j] && area1 > maxArea){
+                maxArea = area1; //maxArea = 1
+            }
+            else if(height[i] > height[j] && area2 > maxArea){
+                maxArea = area2
+            }
+        }
+    }
+    //return max Area
+    return maxArea;
+};
+console.log("maxArea4:", maxArea4([1,1]))
+
