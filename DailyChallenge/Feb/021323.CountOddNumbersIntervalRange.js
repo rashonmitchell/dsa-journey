5eb5fc3dd3e79bdc8bd08aa398eb7ca5fdc6909a
GPV <gvill005@gmail.com>
2023-02-14 20:44:36 -0800

    Co-authored-by: Rashón (Teddy) Mitchell <rashonmitchell@users.noreply.github.com>

--- DailyChallenge/Feb/021323.CountOddNumbersIntervalRange.js
+++ DailyChallenge/Feb/021323.CountOddNumbersIntervalRange.js
@@ -24,23 +24,58 @@
  * 
  */
 
-
 /**
  * @param {number} low
  * @param {number} high
  * @return {number}
  */
-
+                                                                                            /*
+                                                                                                    3,4,5,6,7
+                                                                                            */
 
 const countOdds = function(low, high) {
-    let result = 0;
-    const isOdd = (num) => num % 2 !== 0;
+
+    let result = 0;                             // result = 3
+
+    const isOdd = (num) => num % 2 !== 0;       // if isOdd(low) is odd, result ++
     if(isOdd(low)) result++;
 
-    const num = Math.floor((high - low) / 2);
-    result += num;
+    const num = Math.floor((high - low) / 2); // 7 - 3 / 2 = 2
+    result += num;                            // result += 2;
 
     // if high is odd and not equal to low + 2 * num bc that would mean there are an even number of odds between low and high
-    if(high > low + 2 * num && isOdd(high)) result++; 
+    if(high > low + 2 * num && isOdd(high)) result++;    // 3 + 2 * 3 and isOdd(7) is odd then result++
+
     return result;
+
 };
+
+console.log("countOdds:", countOdds(3, 7))
+
+/*
+Understand the Problem:
+    return the count of numbers that are odd between low and high
+Devise a Plan:
+    set empty variable for count
+    loop through and see if value modulus 3 equals zero if so count++
+
+Code: Brute Force
+                            low = 3, high = 7
+*/
+
+const countOdds2 = function(low, high) {
+
+    //create counter
+    let result = 0;   //result =
+    //loop
+    while(low <= high){
+        //if modulus of 1 equals zero count++ and then low++
+        if(low % 2 === 1) result++, low++;
+        else low++;
+    }
+    
+    return result;
+
+};
+
+console.log("countOdds2:", countOdds2(3, 7))
