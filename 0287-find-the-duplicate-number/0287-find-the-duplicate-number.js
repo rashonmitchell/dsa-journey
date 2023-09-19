6f64d8b097bb8d3d41a881482d5c376039fc0cea
Rashón (Teddy) Mitchell <rashonmitchell@users.noreply.github.com>
2023-09-19 17:51:30 -0500

    Time: 68 ms (85.45%), Space: 49.9 MB (57.34%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -5,13 +5,37 @@
 
 // O(n) time and O(1) space 
 // floyd's cycle detection algorithm solution
+// const findDuplicate = function(nums) {
+//     const hash = {};
+//     for (let idx = 0; idx < nums.length; idx++) {
+//         if (hash.hasOwnProperty(nums[idx])) {
+//             return nums[idx];
+//         } else {
+//             hash[nums[idx]] = 1;
+//         }
+//     }
+// };
+
+// another
+
+// two pointer solution
 const findDuplicate = function(nums) {
-    const hash = {};
-    for (let idx = 0; idx < nums.length; idx++) {
-        if (hash.hasOwnProperty(nums[idx])) {
-            return nums[idx];
-        } else {
-            hash[nums[idx]] = 1;
-        }
+    let slow = nums[0];
+    let fast = nums[0];
+
+    // Phase 1: Find the intersection point of the two pointers
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow !== fast);
+
+    // Phase 2: Find the entrance to the cycle
+    let ptr1 = nums[0];
+    let ptr2 = slow;
+    while (ptr1 !== ptr2) {
+        ptr1 = nums[ptr1];
+        ptr2 = nums[ptr2];
     }
+
+    return ptr1;
 };
